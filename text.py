4717bc6bc9b6831073569c9c76cdd7bd79202307
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-05 20:20:15 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,40 @@
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton, Message 
+
+class Text(object):
+    START = """"**Hai👋 {} ,
+A Simple PDsik Uploader Bot. It Can Upload Link To PDisk.
+Send Me Any Direct Link Or YouTube or Video Link I Will Upload To PDisk And Give Direct Pdisk Link
+/help for More detail
+Made With❤BY @MyTestBotZ**
+"""
+
+    HELP = """**How to Use Me...
+⦿ Its Easy to Use me {} **
+✪ » Send me Any Direct Link or YouTube Link
+✪ »i will upload to PDisk & Give Link
+➠ **If you want Upload Telegram file,Videos to PDisk**
+✪ » First Send any File to <a href="https://telegram.me/Link4filesbot">@Link4Filesbot</a> to generate Direct Link
+✪ » Copy Generated Link and Paste here...
+✪ » Violaaaa.... Done 🥳🥳🥳🥳
+"""
+
+    ABOUT = """➠ **My Name : PDisk Upload bot**
+➠ **Creator : <a href="https://telegram.dog/oo7robot">This Person</a>**
+➠ Credits : <code>Everyone in this journey</code>
+➠ Language : <code>Python3.9.6</code>
+➠ Library : <a href="https://docs.pyrogram.org/">Pyrogram v1.2.9</a>
+➠ Cloned From : <b>Paritosh Kumar</b> Source code
+➠ Source Code : 
+➠ Server : <b>Heroku</b>
+➠ Build Status : <b>Stable V1</b>
+"""
+    SB = InlineKeyboardMarkup(
+      [[
+        InlineKeyboardButton("⭕️ Updates Channel ⭕️", url="https://t.me/MyTestBotZ")
+      ],[
+        InlineKeyboardButton("👨‍💻 Creator", url="https://t.me/OO7ROBOT"),
+        InlineKeyboardButton("🍿 OtherBots", url="https://telegram.me/mybotzlist")
+     ]]
+    )
+
+ 
